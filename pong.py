d253c44f7f6761f0af03c68b4b362d94533af585
MI <87429150+Hazard-map@users.noreply.github.com>
2021-07-21 22:26:15 +0900

    first commit

---
+++ pong.py
@@ -0,0 +1,58 @@
+import sys
+
+import pygame
+from pygame.locals import *
+
+# colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+# sizes
+SCREEN_SIZE = (400, 300)
+
+
+class Position:
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+
+	def set_position(self, *position):
+		length = len(position)
+		if length == 2:
+			self.x = position[0]
+			self.y = position[1]
+		elif length == 1 and type(position[0]) == Position:
+			p = position[0]
+			self.x = p.x
+			self.y = p.y
+		else:
+			raise Exception("wrong argument")
+
+	def get_postion(self):
+		return (self.x, self.y,)
+
+
+def draw_two_rectangles(screen):
+	pygame.draw.rect(screen, WHITE, (20, 70, 10, 180))
+	pygame.draw.rect(screen, WHITE, (370, 70, 10, 180))
+
+
+def main():
+	pygame.init()
+	screen = pygame.display.set_mode(SCREEN_SIZE)
+
+	while True:
+		screen.fill(BLACK)
+
+		draw_two_rectangles(screen)
+
+		pygame.display.update()
+
+		for event in pygame.event.get():
+			# 閉じるボタンおしたら終了する
+			if event.type == QUIT:
+				pygame.quit()
+				sys.exit()
+
+
+if __name__ == '__main__':
+	main()
